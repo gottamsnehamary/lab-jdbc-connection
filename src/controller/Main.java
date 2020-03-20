b88a35b8a72af219dc689db4c900a31f42169ee8
gottamsnehamary <gottamsneha21@gmail.com>
2020-03-20 23:04:53 +0530

    lab-jdbc-connection

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,20 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
 	// Fill your code
-		
+		ConnectionManager cm =new ConnectionManager();
+		Connection con =cm.getConnection();
+		if(con!=null)
+			System.out.println("Established");
+		else
+			System.out.println("check your connection" );
+	
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,47 @@
 package utility;
 
-
-
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectionManager {
 	
+	public Connection getConnection() throws Exception 
+	{
+		/*Class.forName("oracle.jdbc.OracleDriver");
+		
+		Connection con= null;
+		
+	con =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl","SYSTEM","Edition11g");
+	
+		return con;*/
+		
+Properties prop=loadPropertiesFile();
+{
+		
+		String driver = prop.getProperty("driver");
+		String url= prop.getProperty("url");
+		String user=prop.getProperty("username");
+		String password=prop.getProperty("password");
+		 Class.forName(driver);
+		 Connection con;
+		 con=DriverManager.getConnection(url,user,password);
+		 
+		 return con;
+		 
+
+	}
+	}
+		
+public static Properties loadPropertiesFile() throws Exception
+{
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close();
+		return prop;
+	
+} 
 	
 }
